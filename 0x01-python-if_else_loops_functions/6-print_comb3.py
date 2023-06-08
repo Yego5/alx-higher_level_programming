37b1b8dfc28f0b01827efd97aadd62efda986827
yego5 <shachz.github@gmail.com>
2023-06-07 20:00:09 -0700

    6-print_comb3.py the1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for nam1 in range(0, 10):
-    for nam2 in range(nam1 + 1, 10):
-        if nam1 == 8 and nam2 == 9:
-            print("{}{}".format(nam1, nam2))
-    	else:
-            print("{}{}".format(nam1, nam2), end=", ")
+for nam2 in range(nam1 + 1, 10):
+    if nam1 == 8 and nam2 == 9:
+        print("{}{}".format(nam1, nam2))
+    else:
+        print("{}{}".format(nam1, nam2), end=", ")
