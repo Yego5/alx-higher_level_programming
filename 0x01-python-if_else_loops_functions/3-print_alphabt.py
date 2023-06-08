90d2bbc656c2ae6562dc0d35b2d44f3197e4a25c
yego5 <shachz.github@gmail.com>
2023-06-07 18:53:53 -0700

    3-print_alphabt.py12

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-for character in range(ord('a'), ord('z') + 1):
-    if character != 101 and character != 113:
-        print("{:c}".format(character), end='')
-
-print()
+for char in range(97, 123):
+    if (char != 101 and char != 113):
+        print("{:c}".format(char), end='')
 
