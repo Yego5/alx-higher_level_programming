bddb65f975da78fd8afc1a6b45d36ed7ccf60ab3
yego5 <shachz.github@gmail.com>
2023-06-12 15:53:18 -0700

    Replace in a copy 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# 4-nw_in_list.py
+# 4-new_in_list.py
 
-def nw_in_list(nw_list, nw_idx, nw_element):
-	"""Replace an element in a copied list at a specific position."""
-	if nw_idx < 0 or nw_idx > (len(nw_list) - 1):
-    		return nw_list
 
-	copy = [x for x in nw_list]
-	copy[nw_idx] = nw_element
-	return copy
+def new_in_list(new_list, new_idx, new_element):
+    """Replace an element in a copied list at a specific position."""
+    if new_idx < 0 or new_idx > (len(new_list) - 1):
+        return new_list
+
+    copy = [x for x in new_list]
+    copy[new_idx] = new_element
+    return copyy
