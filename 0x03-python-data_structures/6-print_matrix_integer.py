01fcb6ea3a7db309f9e841b5d8c074ad960d9fca
yego5 <shachz.github@gmail.com>
2023-06-13 13:37:04 -0700

    limited to 79 chr

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(new_matrix=[[]]):
     for new_row in new_matrix:
         for new_col in new_row:
             print("{:d}".format(new_col), end=" " if new_col != new_row[-1] else "")
-        print()
+    print()
