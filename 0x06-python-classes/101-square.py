693bedc0a9412d852a77dd1221bec6989adf2ea5
yego5 <shachz.github@gmail.com>
2023-06-28 16:46:42 -0700

    101-square.py2

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,7 +5,7 @@ class Square:
 
     @property
     def size(self):
-        return self.__s_size
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -13,7 +13,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__s_size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -24,25 +24,35 @@ class Square:
         if (
             not isinstance(value, tuple)
             or len(value) != 2
-            or not all(isinstance(numb, int) for numb in value)
-            or not all(numb >= 0 for numb in value)
+            or not all(isinstance(num, int) for num in value)
+            or not all(num >= 0 for num in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        return self.__s_size ** 2
+        return self.__size ** 2
 
     def my_print(self):
-        if self.__s_size == 0:
+        if self.__size == 0:
             print("")
             return
 
         for _ in range(self.__position[1]):
             print("")
 
-        for _ in range(self.__s_size):
-            print(" " * self.__position[0] + "#" * self.__s_size)
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
 
     def __str__(self):
-        return self.my_print()
+        result = ""
+        if self.__size == 0:
+            return result
+
+        for _ in range(self.__position[1]):
+            result += "\n"
+
+        for _ in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size + "\n"
+
+        return result
