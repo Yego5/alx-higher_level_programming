b945b95dec5b52613a7f92c7a115f4d858d8216b
yego5 <shachz.github@gmail.com>
2023-07-04 12:43:25 -0700

    1-rectangle.py 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,30 +3,43 @@
 
 
 class Rectangle:
-	def __init__(self, width=0, height=0):
-    	self.__width = width
-    	self.__height = height
+    """Represent a rectangle."""
 
-	@property
-	def width(self):
-    	return self.__width
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
 
-	@width.setter
-	def width(self, value):
-    	if not isinstance(value, int):
-        	raise TypeError("width must be an integer")
-    	if value < 0:
-        	raise ValueError("width must be >= 0")
-    	self.__width = value
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self._width = 0
+        self._height = 0
+        self.width = width
+        self.height = height
 
-	@property
-	def height(self):
-    	return self.__height
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self._width
 
-	@height.setter
-	def height(self, value):
-    	if not isinstance(value, int):
-        	raise TypeError("height must be an integer")
-    	if value < 0:
-        	raise ValueError("height must be >= 0")
-    	self.__height = value
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
