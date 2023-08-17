abc5752d9c03c6309f8bb3cfc7335be71f83888d
yego5 <shachz.github@gmail.com>
2023-08-17 13:08:36 -0700

    16-no_link.sql 2

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
--- it lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in the MySQL server.
-SELECT score,COUNT(*) as `number`FROM second_table GROUP BY score ORDER BY score DESC;
+-- The lists all records of the table second_table of the database hbtn_0c_0 in MySQL server.
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
