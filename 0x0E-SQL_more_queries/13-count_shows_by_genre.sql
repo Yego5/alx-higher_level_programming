a9d1d5cdc250f7f22384937933380a92a129ad1d
yego5 <shachz.github@gmail.com>
2023-08-17 19:17:03 -0700

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- The code lists all genres from the database hbtn_0d_tvshows along with the number of
+-- Shows linked to each.
+-- It does not display genres without linked shows.
+-- also records are ordered by descending number of shows linked.
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
+-- @shachz.
