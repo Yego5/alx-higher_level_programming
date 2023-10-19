09c8e84e3e7d4a1db040aac38a8b3b094442a31d
yego5 <shachz.github@gmail.com>
2023-10-19 16:35:31 -0700

    citites

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
        state_id INT NOT NULL,
        FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
        name VARCHAR(256) NOT NULL);
---@shadrack
+--@shadrack redo
