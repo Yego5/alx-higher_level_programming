430de232f8866a9d18f57604d7f95acd923108d3
yego5 <shachz.github@gmail.com>
2023-11-24 16:32:50 -0800

    2 filter state 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,6 +3,7 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
@@ -13,4 +14,4 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
     cur.close()
-    db.close() 
+    db.close()
